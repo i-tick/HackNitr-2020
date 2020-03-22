904b987d42617f979c1aaae0ac468ae3e70ffce2
i-tick <aitikdandapat@gmail.com>
2020-03-22 23:36:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vmac/WatBot/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.example.vmac.WatBot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.vmac.WatBot.R;
+
+public class Main2Activity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        b1 = (Button)findViewById(R.id.confirm);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
